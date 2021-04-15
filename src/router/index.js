3075ca41757d727344113c657777370083484472
yuguangzhou <yuguangzhou@zjjcnt.com>
2021-04-15 09:36:39 +0800

    '基础模版搭建'

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+/*
+ * @Author: yuguangzhou
+ * @Date: 2021-04-13 09:52:36
+ * @LastEditTime: 2021-04-13 09:53:35
+ * @LastEditors: yuguangzhou
+ * @Description:
+ */
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<!--
+ * @Author: yuguangzhou
+ * @Date: 2021-04-13 09:52:36
+ * @LastEditTime: 2021-04-13 10:50:43
+ * @LastEditors: yuguangzhou
+ * @Description: 
+-->
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
